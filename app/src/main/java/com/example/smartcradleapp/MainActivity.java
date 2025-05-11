37140253951dee8852badf636a9b53ff3c537dbc
Rowell2002 <chethanarowell2002@gmail.com>
2025-05-11 12:57:29 +0530

    Added a dependency

--- app/src/main/java/com/example/smartcradleapp/MainActivity.java
+++ app/src/main/java/com/example/smartcradleapp/MainActivity.java
@@ -1,24 +1,41 @@
 package com.example.smartcradleapp;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.google.firebase.database.*;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView alertText;
+    FirebaseDatabase database;
+    DatabaseReference ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        alertText = findViewById(R.id.alertText);
+
+        database = FirebaseDatabase.getInstance();
+        ref = database.getReference("alerts");
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                StringBuilder alerts = new StringBuilder();
+                for (DataSnapshot child : snapshot.getChildren()) {
+                    alerts.append(child.getKey()).append(": ")
+                            .append(child.getValue().toString()).append("\n");
+                }
+                alertText.setText(alerts.toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                alertText.setText("Error: " + error.getMessage());
+            }
         });
     }
 }
