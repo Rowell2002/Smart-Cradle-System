0ec7ad7375b2acddaf680e6e329bc2f7d74599f6
Rusiru <liyanagerusiru12@gmail.com>
2025-05-28 22:31:51 +0530

    Added more imports

--- app/src/main/java/com/example/smartcradleapp/MainActivity.java
+++ app/src/main/java/com/example/smartcradleapp/MainActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 }
