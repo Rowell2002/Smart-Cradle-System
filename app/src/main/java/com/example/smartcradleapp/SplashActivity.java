62ba8770539caefd3a09d7024054b9c1516de767
anushkadahanayake <anushkadahanayake1@gmail.com>
2025-05-29 15:55:37 +0530

    update Splash Activity java class

--- app/src/main/java/com/example/smartcradleapp/SplashActivity.java
+++ app/src/main/java/com/example/smartcradleapp/SplashActivity.java
@@ -1,24 +1,24 @@
 package com.example.smartcradleapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DISPLAY_LENGTH = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
